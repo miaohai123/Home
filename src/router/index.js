652ed4c57a0c06c6318b40319f3c85bd2e03fa52
妙海 <13040854+miaohai@user.noreply.gitee.com>
2026-06-23 16:02:28 +0800

    修改路由模式Hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // src/router/index.js
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   { path: "/", name: "Home", component: () => import("@/views/Home.vue") },
