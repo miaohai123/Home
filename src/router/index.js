6ffaf0b98872b53fe99b8fe1407a450b7821a993
妙海 <13040854+miaohai@user.noreply.gitee.com>
2026-06-23 16:19:56 +0800

    资源路径修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // src/router/index.js
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   { path: "/", name: "Home", component: () => import("@/views/Home.vue") },
